332098db2d9ceb7832b8345b8a76aee336961391
ryudo87 <52835839+ryudo87@users.noreply.github.com>
2026-04-07 18:49:47 +0000

    Checkpoint from VS Code for cloud agent session

---
+++ build/CMakeFiles/SORInterviewPrep.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SORInterviewPrep.dir/src/main.cpp.o"
+  "CMakeFiles/SORInterviewPrep.dir/src/main.cpp.o.d"
+  "SORInterviewPrep"
+  "SORInterviewPrep.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SORInterviewPrep.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
